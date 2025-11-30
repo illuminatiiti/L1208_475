ad6159abe33cae074a639f654206e38bdc053b8f
anna_mrscucumber <annaektb@gmail.com>
2025-11-30 18:05:03 +0500

    Добавлены основной и заголовочный файлы

---
+++ L1208_475.hpp
@@ -0,0 +1,15 @@
+#ifndef L1208_475_HPP
+#define L1208_475_HPP
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+struct point //точка с двумя координатами
+{
+	double x,y;
+};
+struct tsp_instance //
+{
+
+};
+#endif

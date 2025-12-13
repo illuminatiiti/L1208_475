651666b3b67d440667695fea8a3e8c4bead9a1f9
anna_mrscucumber <annaektb@gmail.com>
2025-12-14 00:04:01 +0500

    добавлены прототипы функций и отредактировано оформление в заголовочном файле

--- L1208_475.hpp
+++ L1208_475.hpp
@@ -1,23 +1,26 @@
 #ifndef L1208_475_HPP
 #define L1208_475_HPP
 
-// точка x,y
+/* точка x,y */ 
 typedef struct {
     int x, y;    
 } point;
 
-// тип для набора точек на котором будем решать tsp (traveling salesman problem)
+/* тип для набора точек на котором будем решать tsp (traveling salesman problem) */ 
 typedef struct {
-    int n;         // сколько точек в проблеме         
-    point p[1002]; // массив точек  
+    int n;         /* сколько точек в проблеме */          
+    point p[1002]; /* массив точек */   
 } tsp_instance;
 
-//структура для представления решения 
+/* структура для представления решения */ 
 typedef struct {
-    double cost;   //стоимость решения
-    int n;         //количество перестановок
-    int p[1002];   //массив индексов
+    double cost;   /* стоимость решения */
+    int n;         /* количество перестановок */
+    int p[1002];   /* массив индексов */
 } tsp_solution;
 
+void solution_count_update(tsp_solution* s, tsp_instance* t);
+void anneal(tsp_instance* t, tsp_solution* s);
+void repeated_annealing(tsp_instance* t, int nsamples, tsp_solution* bestsol);
 
 #endif

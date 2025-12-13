76527fa444cab4035b8b13085865d46e7ad20120
anna_mrscucumber <annaektb@gmail.com>
2025-12-13 21:20:09 +0500

    добавлен заголовочный файл с описанием структур используемых для решения задачи коммивояжера

---
+++ L1208_475.hpp
@@ -0,0 +1,23 @@
+#ifndef L1208_475_HPP
+#define L1208_475_HPP
+
+// точка x,y
+typedef struct {
+    int x, y;    
+} point;
+
+// тип для набора точек на котором будем решать tsp (traveling salesman problem)
+typedef struct {
+    int n;         // сколько точек в проблеме         
+    point p[1002]; // массив точек  
+} tsp_instance;
+
+//структура для представления решения 
+typedef struct {
+    double cost;   //стоимость решения
+    int n;         //количество перестановок
+    int p[1002];   //массив индексов
+} tsp_solution;
+
+
+#endif
